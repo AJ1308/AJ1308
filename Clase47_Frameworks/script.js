087616c6deea948e33081cd18715bf132b6d846d
Angel Mazariegos <mazariegosnangelece@gmail.com>
2022-01-21 17:06:49 -0600

    21/1

--- Clase47_Frameworks/script.js
+++ Clase47_Frameworks/script.js
@@ -1,6 +1,23 @@
 var app = new Vue({
     el: "#app",
+
     data: {
-        message: 'Hello World! '
+        message: "Ahora: " + new Date(),
+        seen: false,
+        tasks: [],
+        newTask: "Nueva tarea"
+    },
+    created: function() {
+        if ( localStorage.getItem("tasks") === null ) {
+            this.tasks = []
+        } else {
+            this.tasks = JSON.parse(localStorage.getItem("tasks"))
+        }
+    },
+    methods: {
+        addTask: function() {
+            this.tasks.push({"text": this.newTask})
+            localStorage.setItem("tasks", JSON.stringify(this.tasks))
+        }
     }
 })
